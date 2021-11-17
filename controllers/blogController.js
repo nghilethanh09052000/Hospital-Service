851733d5bb722c9b2fd6247b006cf619f038a8b0
ThanhNghi <nghilt19411@gmail.com>
2021-11-17 11:10:44 +0700

    add gitignore

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -7,12 +7,16 @@ const blog_homepage=(req,res)=>{
 const login_get=(req,res)=>{
     res.render('login')
 }
+
 const register_get=(req,res)=>{
     res.render('register')
 }
+//const register_post
+
 module.exports = {
     blog_homepage,
     login_get,
-    register_get 
+    register_get,
+    register_post
     
   }
